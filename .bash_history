905025da59ff10af737a3ecdb5a3b1a221b16c15
Eugene <eugeneyvt@yandex.ru>
2023-09-12 23:50:47 +0300

    Updated environments on production

---
+++ .bash_history
@@ -0,0 +1,4 @@
+cd /home/ugolok/public_html/frontend
+nano next.config.js
+sudo lsof -i 49600
+exit
--- public_html/frontend/next.config.js
+++ public_html/frontend/next.config.js
@@ -1,11 +1,9 @@
+require('dotenv').config();
+
 const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    host: process.env.HOST || '0.0.0.0',
-    port: process.env.PORT || 49600
-  }
 };
 module.exports = nextConfig;
