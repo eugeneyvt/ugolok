f17d59e2fd72b9f83c2ede5dec97c383db37bce0
eugeneyvt <143550398+eugeneyvt@users.noreply.github.com>
2023-09-12 20:06:22 +0000

    Deleted all the unnecessary files & modified to use env variables

--- public_html/frontend/next.config.js
+++ public_html/frontend/next.config.js
@@ -3,5 +3,9 @@ const nextConfig = {
     API_URL: process.env.API_URL,
   },
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 49600
+  }
 };
 module.exports = nextConfig;
