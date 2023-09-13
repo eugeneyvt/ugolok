6bf7700f5f346eef38f47fd71be1665ac0d539a5
eugeneyvt <143550398+eugeneyvt@users.noreply.github.com>
2023-09-13 23:34:45 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Make sure the private key doesn't have any formatting issues and is usable
+echo "$PRIVATE_KEY" > /tmp/deploy_key
+chmod 600 /tmp/deploy_key
+
 # Use SSH to connect to your server and deploy
-ssh -i "$PRIVATE_KEY" $USER@$HOST <<EOF
+ssh -o StrictHostKeyChecking=no -i /tmp/deploy_key $USER@$HOST <<EOF
+
   # Pull updates
   cd /home/ugolok
   git pull origin main
