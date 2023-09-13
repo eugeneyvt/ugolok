2f3e24bf8c056724c0abfed710b31e75f09edd3a
eugeneyvt <143550398+eugeneyvt@users.noreply.github.com>
2023-09-14 01:58:31 +0300

    deploy.sh: changed the startup order

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,14 @@ ssh -o StrictHostKeyChecking=no -i /tmp/deploy_key $USER@$HOST <<EOF
   cd /home/ugolok
   git pull origin main
 
+  # Deploying API
+  cd /home/ugolok/domains/api.ugolok.shop/public_html/API
+  npm install
+  pm2 restart u-api
+  
   # Deploying Frontend
   cd /home/ugolok/public_html/frontend
   npm install
   pm2 restart u-front
 
-  # Deploying API
-  cd /home/ugolok/domains/api.ugolok.shop/public_html/API
-  npm install
-  pm2 restart u-api
 EOF
