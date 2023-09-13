eff0a53f0ec7e6a72b2c2eb1191b6446620041aa
eugeneyvt <143550398+eugeneyvt@users.noreply.github.com>
2023-09-13 21:47:16 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,16 @@
 
 # Use SSH to connect to your server and deploy
 ssh -i "$PRIVATE_KEY" $USER@$HOST <<EOF
-  # Navigate to main directory and pull updates
-  cd /home/ugolok
+  # Pull updates
   git pull origin main
 
   # Deploying Frontend
-  cd /home/ugolok/public_html/frontend
+  cd public_html/frontend
   npm install
   pm2 restart u-front
 
   # Deploying API
-  cd /home/ugolok/domains/api.ugolok.shop/public_html/API
+  cd domains/api.ugolok.shop/public_html/API
   npm install
   pm2 restart u-api
 EOF
